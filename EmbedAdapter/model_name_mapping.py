8681b19cd88910617ca7b7bfb881b89c02142962
LC1332 <chengli.thu@gmail.com>
2023-12-15 09:03:15 +0800

    add large

--- EmbedAdapter/model_name_mapping.py
+++ EmbedAdapter/model_name_mapping.py
@@ -1,6 +1,8 @@
 MODEL_NAME_MAPPING = {
     "bge-small-zh-15": "BAAI/bge-small-zh-v1.5",
     "BAAI/bge-small-zh-v1.5":"BAAI/bge-small-zh-v1.5",
+    "bge-large-zh-15": "BAAI/bge-large-zh-v1.5",
+    "BAAI/bge-large-zh-v1.5":"BAAI/bge-large-zh-v1.5",
     "bge-base-zh-15": "BAAI/bge-base-zh-v1.5",
     "BAAI/bge-base-zh-v1.5":"BAAI/bge-base-zh-v1.5",
     "openai":"openai"
